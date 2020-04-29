4338060c1500579bdc0f0ab40c1327c4ac710783
远方 <deanyang1996@126.com>
2020-04-29 23:07:12 +0800

    day2 增加登陆

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 
 
 import React from "react";
+import axios from 'axios';
 
 export default class Login extends React.Component{
 
@@ -13,20 +14,39 @@ export default class Login extends React.Component{
             },
             flag: false // indicate whether clicked on login button
         };
-        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleChangeUsername = this.handleChangeUsername.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         // this.handleLogout = this.handleLogout.bind(this);
     }
 
-    handleInputChange(event) {
-        const {name, value} = event.target;
+    // 做了修改，username和password分别绑定不同的点击事件
+    handleChangeUsername(event) {
+        let username = event.target.value;
+        // const {name, value} = event.target;
         // this.state.user[name] = value;
+        // this.setState({
+        //     user: {
+        //         ...this.state.user,
+        //         [name]: value
+        //     }
+        // });
         this.setState({
             user: {
                 ...this.state.user,
-                [name]: value
+                username:username
             }
-        });
+        })
+    }
+
+    handleChangePassword(event){
+        let password = event.target.value;
+        this.setState({
+            user: {
+                ...this.state.user,
+                password:password
+            }
+        })
     }
 
     handleSubmit(event) {
@@ -34,6 +54,25 @@ export default class Login extends React.Component{
         this.setState(
             {flag:true}
         );
+
+        let formData=new FormData();
+        // 请求头
+        let header = {     
+          headers: { 'content-type': 'multipart/form-data' }
+        };
+
+        // form表单必须使用formdata提交请求
+        formData.append("username",this.state.user.username);
+        formData.append("password",this.state.user.password);
+
+        // 请求后端接口 进行登陆 form表单登陆
+        axios.post("/api/login",formData,header).then(res=>{
+            if(res.data.success){
+                alert("登陆成功！")
+            }else{
+                alert("登陆失败！")
+            }
+        })
         // this.props.login(this.state.user);// this line will finish immediately
     }
 
@@ -50,7 +89,7 @@ export default class Login extends React.Component{
                                        className="form-control"
                                        name="username"
                                        value={this.state.user.username}
-                                       onChange={this.handleInputChange}
+                                       onChange={this.handleChangeUsername}
                                 />
                             </label>
                         </div>
@@ -61,7 +100,7 @@ export default class Login extends React.Component{
                                        className="form-control"
                                        name="password"
                                        value={this.state.user.password}
-                                       onChange={this.handleInputChange}
+                                       onChange={this.handleChangePassword}
                                 />
                             </label>
                         </div>
